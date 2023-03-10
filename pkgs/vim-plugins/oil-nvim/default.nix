4f439ae10cfd395dac4ad922a1a631ba9c658f13
Giovanni Nappi <giovanni.nappi@gmail.com>
2023-03-10 11:49:47 +0100

    chore(nvim): update oil and telescope-heading plugins

--- pkgs/vim-plugins/oil-nvim/default.nix
+++ pkgs/vim-plugins/oil-nvim/default.nix
@@ -1,13 +1,13 @@
 { vimUtils, fetchFromGitHub }:
 vimUtils.buildVimPlugin {
   pname = "oil-nvim";
-  version = "2023-02-23";
+  version = "2023-03-07";
   dontBuild = true;
   src = fetchFromGitHub {
     owner = "stevearc";
     repo = "oil.nvim";
-    rev = "647c2d316c0c22dcd3908be21de1c298e8133b8f";
-    sha256 = "0yl1jlbmdlw99b0nq98l5hyra13ifqhw57l0xn87g2myflxcpybv";
+    rev = "33d5701a8de02bdba171d0795e4422e002f61742";
+    sha256 = "1abxbg5r82cn2alm8iq4n8hil41913f9wawm7sswsyv3nxl55a63";
   };
   meta.homepage = "https://github.com/stevearc/oil.nvim";
 }
--- pkgs/vim-plugins/telescope-heading-nvim/default.nix
+++ pkgs/vim-plugins/telescope-heading-nvim/default.nix
@@ -1,13 +1,13 @@
 { vimUtils, fetchFromGitHub }:
 vimUtils.buildVimPlugin {
   pname = "telescope-heading-nvim";
-  version = "2022-12-04";
+  version = "2023-03-02";
   dontBuild = true;
   src = fetchFromGitHub {
     owner = "crispgm";
     repo = "telescope-heading.nvim";
-    rev = "b1969c1b1b3de52c45b4c440e0784c33b5814954";
-    sha256 = "1ljvigvii74bvfx8952xar5qsb236krhxa7yhv1fll297mxcj9ca";
+    rev = "814faa3a4e35162cdcbfe8e7cd4f0b47015da089";
+    sha256 = "1rrz4abmljk6c6mnzvmximqih68w2a7sd9w55d9kwgfb6nddnl4l";
   };
   meta.homepage = "https://github.com/crispgm/telescope-heading.nvim";
 }
